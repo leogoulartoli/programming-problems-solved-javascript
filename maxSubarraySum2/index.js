4d6cb6562c2eddffc48b6133ab76591c1c18bcb0
Leonardo Goulart de Oliveira <leogoulartoli@gmail.com>
2023-04-04 12:52:07 -0300

    adjustments

--- maxSubarraySum2/index.js
+++ maxSubarraySum2/index.js
@@ -1,5 +1,6 @@
-// Sliding Window - maxSubarraySum
-// Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
+
+// Given an array of integers and a number, write a function called maxSubarraySum, 
+//which finds the maximum sum of a subarray with the length of the number passed to the function.
 
 // Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
 
